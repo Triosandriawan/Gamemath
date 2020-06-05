7abd6ec4e30158127f1f8b4b8797f6d59b77c413
Triosandriawan <65974603+Triosandriawan@users.noreply.github.com>
2020-06-05 22:28:59 +0700

    Add files via upload

---
+++ old_player.php
@@ -0,0 +1,23 @@
+<?php
+//Menjalankan Session
+session_start();
+//Output Pemain yang sudah terdaftar
+echo "Hallo " . $_SESSION['nama_user'] . ", Selamat datang kembali dalam permainan ini!!!<br>";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OldPlay</title>
+</head>
+<body class="content">
+    <!-- Form Memulai Game baru -->
+    <form action='gameplay.php' method='post'>
+        <input type='submit' name='gameplay' value='Start Game'>
+    </form>
+    <!-- Link untuk kembali ke halaman index -->
+    <p>Bukan Anda? <a href="index.php">(klik di sini)</a></p>
+</body>
+</html>
